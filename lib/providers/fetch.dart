0c81eef6ee9ac2959269e7e9b3c683856849d870
gosu4991 <87606753+gosu4991@users.noreply.github.com>
2021-08-04 13:42:16 -0700

    Delete lib/providers directory

--- lib/providers/fetch.dart
+++
@@ -1,8 +0,0 @@
-/*Future getgiaghe() async {
-  var response = await http.get(Uri.parse(url));
-  final obj = json.decode(response.body);
-  print(json.decode(response.body));
-  return obj;
-}*/
-
-
